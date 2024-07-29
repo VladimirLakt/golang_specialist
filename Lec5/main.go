bcf022bcd71fc425db723d723a2d14e4d9969c1f
Vladimir <lact.v@yandex.ru>
2024-07-29 21:31:59 +0700

    Lec 5 29-07-2024

--- Lec5/main.go
+++ Lec5/main.go
@@ -2,41 +2,63 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	// классический условный оператор
-	// if condition {
-	// 		body
-	// } else {
-	// }
-
-	var value int
-	fmt.Scan(&value)
-	if value%2 == 0 {
-		fmt.Println("The number", value, "is even")
-
-	} else {
-		fmt.Println("The number", value, "is odd")
-	}
-
-	// // if condition1 {
-
-	// // } else if {
-	// // 	...
+	// // классический условный оператор
+	// // if condition {
+	// // 		body
 	// // } else {
-
 	// // }
 
-	var color string
-	fmt.Scan(&color)
-	if strings.Compare(color, "green") == 0 {
-		fmt.Println("Color is green")
-	} else if strings.Compare(color, "red") == 0 {
-		fmt.Println("Color is red")
+	// var value int
+	// fmt.Scan(&value)
+	// if value%2 == 0 {
+	// 	fmt.Println("The number", value, "is even")
+
+	// } else {
+	// 	fmt.Println("The number", value, "is odd")
+	// }
+
+	// // // if condition1 {
+
+	// // // } else if {
+	// // // 	...
+	// // // } else {
+
+	// // // }
+
+	// var color string
+	// fmt.Scan(&color)
+	// if strings.Compare(color, "green") == 0 {
+	// 	fmt.Println("Color is green")
+	// } else if strings.Compare(color, "red") == 0 {
+	// 	fmt.Println("Color is red")
+	// } else {
+	// 	fmt.Println("Unknown color")
+	// }
+
+	// // Инициализация в блоке условного оператора
+	// // Блок присваивания только :=
+	// // Инициализируемая переменная видна ТОЛЬКО внутри условия (if, if else, else if)
+
+	// if num := 10; num%2 == 0 {
+	// 	fmt.Println("Четное")
+	// } else {
+	// 	fmt.Println("Нечетное")
+	// }
+
+	// // Ущербно
+
+	var a, b, c float64
+	var D float64 = b*b - 4*a*c
+	fmt.Scan(&a, &b, &c)
+	if D > 0 {
+		fmt.Print("два корня")
+	} else if D == 0 {
+		fmt.Print("один корень")
 	} else {
-		fmt.Println("Unknown color")
+		fmt.Print("корней нет")
 	}
 
 }
